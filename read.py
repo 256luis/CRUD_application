4f16ba831009017ed0d5343c840bc699101300e4
Alphyxia <104832717+Alphyxia@users.noreply.github.com>
2022-05-04 03:30:15 +0800

    Add files via upload

--- read.py
+++ read.py
@@ -1,7 +1,11 @@
-""" palitan mo muna root password argument sa main file para makaconnect sa db"""
+from display import displayData
 # tip lng, macacall itong function everytime may sinelect na option sa filter combobox
 def read(cursor, records):
-    print(cursor)#placeholder lng
-    # para mapadali buhay mo, check mo code sa displayData function kung pano mag clear at insert ng rows sa table('records') ng app
+    cursor = cnt.execute()
+print(cursor)
+for i in cursor:
+    print(i[0]+"    "+str(i[1])+"   "+str(i[2]))
 
-   
+print('')
+    #placeholder lng
+    # para mapadali buhay mo, check mo code sa displayData function kung pano mag clear at insert ng rows sa table('records') ng app
