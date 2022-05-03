e7ab1ce7452aa7a1076918ec51e9b0fba432348c
Andre Aquino <aquino_andre.agustin@yahoo.com>
2022-05-03 22:58:21 +0300

    restricted delete row selection to one only

--- delete.py
+++ delete.py
@@ -1,10 +1,17 @@
 from tkinter import messagebox
 from display import displayData
 
-""" palitan mo muna root password argument sa main file para makaconnect sa db"""
 def delete(db, cursor, records):
-    data = records.item(records.focus(),"values")[1]
-    cursor.execute(f"Delete FROM package WHERE brnch_id = {data}")
+    # get primary key value of selected row
+    try:
+        data = records.item(records.focus(),"values")[0]
+    except: # no selected row
+        messagebox.showerror("Error", "There is no selected row")  
+        return
+    
+    # delete row
+    cursor.execute(f"DELETE FROM PACKAGE WHERE pckge_no = {data}")
+        
     # commit changes
     db.commit() 
 
