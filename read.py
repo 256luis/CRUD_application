7fb491370792c4292b6fd4ef92ae2ffb3b9a5d20
Andre Aquino <aquino_andre.agustin@yahoo.com>
2022-05-04 16:59:44 +0300

    finished merged on styled table

--- read.py
+++ read.py
@@ -12,10 +12,12 @@ def read(cursor, records, filter_cbox, search_bar):
                 # delete current data in table
                 for item in records.get_children():
                     records.delete(item)
-        
+                    
                 # show all data
+                counter = 1 # alternate between odd and even for row color
                 for row in rows:
-                    records.insert("", tkinter.END, values=row)
+                    records.insert("", tkinter.END, values=row, tags=("odd" if counter % 2 != 0 else "even"))
+                    counter = counter + 1                      
             case "Item Code":
                 cursor.execute(f"select * from Package where item_code = {search_bar.get()}")
                 rows = cursor.fetchall()
@@ -24,8 +26,10 @@ def read(cursor, records, filter_cbox, search_bar):
                     records.delete(item)
         
                 # show all data
+                counter = 1 # alternate between odd and even for row color
                 for row in rows:
-                    records.insert("", tkinter.END, values=row)
+                    records.insert("", tkinter.END, values=row, tags=("odd" if counter % 2 != 0 else "even"))
+                    counter = counter + 1    
             case "Batch Code":
                 cursor.execute(f"select * from Package where batch_code = {search_bar.get()}")
                 rows = cursor.fetchall()
@@ -34,8 +38,10 @@ def read(cursor, records, filter_cbox, search_bar):
                     records.delete(item)
         
                 # show all data
+                counter = 1 # alternate between odd and even for row color
                 for row in rows:
-                    records.insert("", tkinter.END, values=row)
+                    records.insert("", tkinter.END, values=row, tags=("odd" if counter % 2 != 0 else "even"))
+                    counter = counter + 1    
             case "Price":
                 cursor.execute(f"select * from Package where pckg_price LIKE {search_bar.get()}")
                 rows = cursor.fetchall()
@@ -44,8 +50,10 @@ def read(cursor, records, filter_cbox, search_bar):
                     records.delete(item)
         
                 # show all data
+                counter = 1 # alternate between odd and even for row color
                 for row in rows:
-                    records.insert("", tkinter.END, values=row)
+                    records.insert("", tkinter.END, values=row, tags=("odd" if counter % 2 != 0 else "even"))
+                    counter = counter + 1    
             case "Manufacture Date":
                 cursor.execute(f"select * from Package where manufac_date = '{search_bar.get()}'")
                 rows = cursor.fetchall()
@@ -54,8 +62,10 @@ def read(cursor, records, filter_cbox, search_bar):
                     records.delete(item)
         
                 # show all data
+                counter = 1 # alternate between odd and even for row color
                 for row in rows:
-                    records.insert("", tkinter.END, values=row)
+                    records.insert("", tkinter.END, values=row, tags=("odd" if counter % 2 != 0 else "even"))
+                    counter = counter + 1    
             case "Preparation Date":
                 cursor.execute(f"select * from Package where prep_date = '{search_bar.get()}''")
                 rows = cursor.fetchall()
@@ -64,8 +74,10 @@ def read(cursor, records, filter_cbox, search_bar):
                     records.delete(item)
         
                 # show all data
+                counter = 1 # alternate between odd and even for row color
                 for row in rows:
-                    records.insert("", tkinter.END, values=row)
+                    records.insert("", tkinter.END, values=row, tags=("odd" if counter % 2 != 0 else "even"))
+                    counter = counter + 1    
             case "Expiration Date":
                 cursor.execute(f"select * from Package where expr_date = '{search_bar.get()}'")
                 rows = cursor.fetchall()
@@ -74,7 +86,9 @@ def read(cursor, records, filter_cbox, search_bar):
                     records.delete(item)
         
                 # show all data
+                counter = 1 # alternate between odd and even for row color
                 for row in rows:
-                    records.insert("", tkinter.END, values=row)
+                    records.insert("", tkinter.END, values=row, tags=("odd" if counter % 2 != 0 else "even"))
+                    counter = counter + 1    
     except: # catch exception
         pass
